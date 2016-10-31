03aa536728b33905f11171a240270621f1e6d175
SurlyMae <surlymae@gmail.com>
2016-10-31 15:14:08 -0600

    feat(core): Add App and Header components in React

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return(
+      <h1>Memory Game</h1>
+    );
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-console.log('yo yo yo');
+'use strict';
+
+import React from 'react';
+import {render} from 'react-dom';
+import {Header} from './components/Header';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById('app'));
