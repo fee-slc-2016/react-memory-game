acf1c1df4a6df6cca52303668588f92a59cd3df9
kelly <kelly.n.waite@gmail.com>
2016-11-07 17:35:16 -0700

    feat(core) Populate card divs with images randomly

--- src/app/components/Board.js
+++ src/app/components/Board.js
@@ -2,18 +2,46 @@
 
 import React from 'react';
 
+const cheeses = ['cheese1.jpeg', 'cheese2.jpg', 'cheese3.png', 'cheese4.png', 'cheese5.jpg'];
+const fisherYates = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+
+const randCheese = (arr, length) => {
+  let newArr = arr.slice();
+  newArr = fisherYates(newArr);
+  newArr = newArr.slice(0, length);
+  let secondArr = newArr;
+  secondArr.map( (c) => newArr.push(c) );
+  newArr = fisherYates(newArr);
+  return newArr;
+};
+
 export default class Board extends React.Component {
 
+
+
   render() {
+
+    const halfBoard = this.props.board.size / 2;
+
+    let randoCheese = randCheese(cheeses, halfBoard);
+    console.log(randoCheese);
+
     return (
       <div className="board">
-        {[...Array(this.props.board.size)].map((c, i, a) =>
+        {randoCheese.map((c, i, a) =>
           <div className="card" key={i + 1}>
-            <img src="./app/images/cheese1.jpeg"/>
+            <img src={"./app/images/card_images/" + c} />
            </div>
         )}
       </div>
-
     );
   }
 }
